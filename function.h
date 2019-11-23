58340a3d7f18119fc18cf1217454f59af40e5b3d
bob <alexxx.iv@yandex.ru>
2019-11-23 12:47:40 +0300

    fix

--- function.h
+++ function.h
@@ -28,12 +28,11 @@ boolean enc_block=false, enc_flag=false, lsb_usb_flag=false,
         rit_flag=false, Button_flag=false, tx_flag=false,
         step_flag=false, rewrite_flag=false, setup_flag=false;
 
-uint8_t SWR_count=0, AVR_count=0,
-        Enc_state, Enc_last,
-        step_count=4, menu_count=0, setup_count=8, xF=1,
-        SI5351_DRIVE_CLK0, SI5351_DRIVE_CLK1, SI5351_DRIVE_CLK2; // 0 ..255
-int8_t enc_move=0, XTAL = 30;
-byte ENC_SPIN = 1;
+uint8_t SWR_count = 0, AVR_count = 0, XTAL = 27, Enc_state, Enc_last;
+uint8_t step_count = 4, menu_count=0, setup_count=8, xF=1;
+uint8_t SI5351_DRIVE_CLK0, SI5351_DRIVE_CLK1, SI5351_DRIVE_CLK2; // 0 ..255
+
+int8_t enc_move = 0, ENC_SPIN = 1;
 
 uint16_t Ftone, uFRD, uREV;
 
